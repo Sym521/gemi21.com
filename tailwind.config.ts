f1ec1c54fcd737eb303cbbe490aa0747a80b980b
Sym521 <hiroki.seyama@outlook.jp>
2024-08-21 00:42:40 +0900

    fix: add customized colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'mustard': '#DDE7A5',
+        'sonavy': '#22185A',
+      },
     },
   },
   plugins: [],
