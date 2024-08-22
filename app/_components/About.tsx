6b94c136faf69451da30fa09382762fdcca839f5
Sym521 <hiroki.seyama@outlook.jp>
2024-08-23 00:33:42 +0900

    feat: add an about me component

--- app/_components/About.tsx
+++ app/_components/About.tsx
@@ -2,8 +2,14 @@
 
 export const About = () => {
     return (
-        <div>
-            <p>Hi! My name is Hiroki Seyama!</p>
+        <div className="my-8 ml-4 flex-col">
+            <h1 className="text-4xl">About me 👋</h1>
+            <span className="text-2xl">
+                <p>Hello! My name is Hiroki Seyama!</p>
+                <p>I am currently a associate student at National Institute of Technology, Oita College.</p>
+                <p>My major is information engenieering.</p>
+                <p>I am curious about web designing, artificial intelligence, and quantum computing.</p>
+            </span>
         </div>
     );
-};
+}
--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,16 +1,19 @@
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
 export const Header = () => {
     return (
         <>
             <div className="flex justify-between bg-mustard">
-                <div className="text-4xl md:text-8xl text-left ml-2 text-sonavy">
-                    <h1>Hiroki</h1>
-                    <h1>Seyama</h1>
+                <div className="text-3xl md:text-7xl text-left ml-2 text-sonavy">
+                    <h1>Hiroki Seyama</h1>
+                    <a href="https://github.com/Sym521">
+                        <GitHubLogoIcon width={40} height={40} />
+                    </a>
                 </div>
-                <div className="flex flex-col justify-end items-end text-2xl md:text-6xl space-y-1 mr-2 text-right text-sonavy list-none">
-                    <Link href={"/about"} className="hover:underline">About</Link>
-                    <Link href={"/"} className="hover:underline">My Works</Link>
+                <div className="flex flex-col justify-end items-end text-3xl md:text-5xl space-y-1 mr-2 text-right text-sonavy list-none">
+                    <Link href={"/"} className="hover:underline">About</Link>
+                    <Link href={"/"} className="hover:underline">Projects</Link>
                     <Link href={"/"} className="hover:underline">Contact</Link>
                 </div>
             </div>
