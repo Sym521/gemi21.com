72b71a419d51508b2a080887c69d2cceacd47ecd
Sym521 <hiroki.seyama@outlook.jp>
2024-10-02 23:32:51 +0900

    fix: add prettier-tailwind to project

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,8 +1,7 @@
-
 export const Footer = () => {
-    return(
-        <div className="mt-auto bg-slate-800">
-            <p className="ml-4">©2024 gemi21.com</p>
-        </div>
-    )
-}
+  return (
+    <div className="mt-auto bg-slate-800">
+      <p className="ml-4">©2024 gemi21.com</p>
+    </div>
+  );
+};
