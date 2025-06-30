ac4ab6c8059740eaec0c6c3b80b8ee7b675651e0
sym521 <hiroki.seyama@outlook.jp>
2025-06-30 14:56:52 +0900

    fix: update mdx-components.tsx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,12 @@
 import type { MDXComponents } from 'mdx/types';
 
 const baseClasses = {
-    h1: "text-4xl md:text-5xl font-bold text-left",
-    h2: "text-3xl md:text-4xl font-semibold text-left",
-    h3: "text-2xl md:text-3xl font-semibold text-left",
-    p: "text-md md:text-lg text-left",
-    ul: "list-disc list-inside text-md md:text-lg",
+    h1: "text-4xl md:text-5xl font-bold text-left text-stone-600",
+    h2: "text-3xl md:text-4xl font-semibold text-left text-stone-600",
+    h3: "text-2xl md:text-3xl font-semibold text-left text-stone-600",
+    p: "text-md md:text-lg text-left text-stone-600",
+    ul: "list-disc list-inside text-md md:text-lg text-stone-600",
+    li: "mb-2 text-md md:text-lg text-stone-600",
 };
 
 const elements = ['h1', 'h2', 'h3', 'p', 'ul'] as const;
