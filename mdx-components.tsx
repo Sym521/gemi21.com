6f5f07a6f46a999c5dea562daee7f4118d4c5480
sym521 <hiroki.seyama@outlook.jp>
2025-06-30 15:03:20 +0900

    fix: delete TagClassfrom mdx-components.tsx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,29 +1,37 @@
 import type { MDXComponents } from 'mdx/types';
 
-const baseClasses = {
-    h1: "text-4xl md:text-5xl font-bold text-left text-stone-600",
-    h2: "text-3xl md:text-4xl font-semibold text-left text-stone-600",
-    h3: "text-2xl md:text-3xl font-semibold text-left text-stone-600",
-    p: "text-md md:text-lg text-left text-stone-600",
-    ul: "list-disc list-inside text-md md:text-lg text-stone-600",
-    li: "mb-2 text-md md:text-lg text-stone-600",
-};
-
-const elements = ['h1', 'h2', 'h3', 'p', 'ul'] as const;
-
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-    const customComponents = Object.fromEntries(
-        elements.map((Tag) => [
-            Tag,
-            ({ children, ...props }: any) => (
-                <Tag className={baseClasses[Tag]} {...props}>
-                    {children}
-                </Tag>
-            ),
-        ])
-    );
     return {
-        ...customComponents,
+        h1: ({ children, ...props }: any) => (
+            <h1 className="text-4xl md:text-5xl font-bold text-left text-stone-600" {...props}>
+                {children}
+            </h1>
+        ),
+        h2: ({ children, ...props }: any) => (
+            <h2 className="text-3xl md:text-4xl font-semibold text-left text-stone-600" {...props}>
+                {children}
+            </h2>
+        ),
+        h3: ({ children, ...props }: any) => (
+            <h3 className="text-2xl md:text-3xl font-semibold text-left text-stone-600" {...props}>
+                {children}
+            </h3>
+        ),
+        p: ({ children, ...props }: any) => (
+            <p className="text-md md:text-lg text-left text-stone-600" {...props}>
+                {children}
+            </p>
+        ),
+        ul: ({ children, ...props }: any) => (
+            <ul className="list-disc list-inside text-md md:text-lg text-stone-600" {...props}>
+                {children}
+            </ul>
+        ),
+        li: ({ children, ...props }: any) => (
+            <li className="mb-2 text-md md:text-lg text-stone-600" {...props}>
+                {children}
+            </li>
+        ),
         ...components,
     };
 }
