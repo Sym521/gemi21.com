e228f3391e5d2b2b82a93ef385ceee5cd76dc9c2
sym521 <hiroki.seyama@outlook.jp>
2025-07-07 09:19:24 +0900

    fix: resume bento-grid.tsx

--- app/_components/ui/bento-grid.tsx
+++ app/_components/ui/bento-grid.tsx
@@ -1,16 +1,25 @@
+import type { ReactNode } from "react";
+
 import { cn } from "@/lib/utils";
 
-export const BentoGrid = ({
-	className,
-	children,
-}: {
+type BentoGridProps = {
+	children: ReactNode;
 	className?: string;
-	children?: React.ReactNode;
-}) => {
+};
+
+type BentoCardProps = {
+	name: string;
+	className: string;
+	background: ReactNode;
+	Icon: React.ElementType;
+	description: string;
+};
+
+const BentoGrid = ({ children, className }: BentoGridProps) => {
 	return (
 		<div
 			className={cn(
-				"mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3",
+				"grid w-full auto-rows-[22rem] grid-cols-3 gap-4",
 				className,
 			)}
 		>
@@ -19,36 +28,33 @@ export const BentoGrid = ({
 	);
 };
 
-export const BentoGridItem = ({
+const BentoCard = ({
+	name,
 	className,
-	title,
+	background,
+	Icon,
 	description,
-	header,
-	icon,
-}: {
-	className?: string;
-	title?: string | React.ReactNode;
-	description?: string | React.ReactNode;
-	header?: React.ReactNode;
-	icon?: React.ReactNode;
-}) => {
-	return (
-		<div
-			className={cn(
-				"group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-neutral-200 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
-				className,
-			)}
-		>
-			{header}
-			<div className="transition duration-200 group-hover/bento:translate-x-2">
-				{icon}
-				<div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
-					{title}
-				</div>
-				<div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-					{description}
-				</div>
-			</div>
+}: BentoCardProps) => (
+	<div
+		key={name}
+		className={cn(
+			"group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
+			// light styles
+			"bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
+			// dark styles
+			"transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+			className,
+		)}
+	>
+		<div>{background}</div>
+		<div className="pointer-events-none flex transform-gpu flex-col gap-1 p-3">
+			<Icon className="h-8 w-8 origin-left transform-gpu text-neutral-700 " />
+			<h3 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
+				{name}
+			</h3>
+			<p className="max-w-lg text-neutral-400">{description}</p>
 		</div>
-	);
-};
+	</div>
+);
+
+export { BentoCard, BentoGrid };
