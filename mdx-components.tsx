49d67fbacbd3f84d0170fa05bb54d6b601e8505b
sym521 <hiroki.seyama@outlook.jp>
2025-07-07 09:54:55 +0900

    feat: add image style

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,17 +3,17 @@ import type { MDXComponents } from 'mdx/types';
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
         h1: ({ children, ...props }: any) => (
-            <h1 className="text-4xl md:text-5xl font-bold text-left text-slate-800 font-Zen_Kaku_Gothic_New" {...props}>
+            <h1 className="text-3xl font-bold text-left text-slate-800 font-Zen_Kaku_Gothic_New" {...props}>
                 {children}
             </h1>
         ),
         h2: ({ children, ...props }: any) => (
-            <h2 className="text-3xl md:text-4xl font-semibold text-left text-slate-800 font-Zen_Kaku_Gothic_New" {...props}>
+            <h2 className="text-2xl font-semibold text-left text-slate-800 font-Zen_Kaku_Gothic_New" {...props}>
                 {children}
             </h2>
         ),
         h3: ({ children, ...props }: any) => (
-            <h3 className="text-2xl md:text-3xl font-semibold text-left text-slate-800 font-Zen_Kaku_Gothic_New" {...props}>
+            <h3 className="text-xl font-semibold text-left text-slate-800 font-Zen_Kaku_Gothic_New" {...props}>
                 {children}
             </h3>
         ),
@@ -36,7 +36,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
             <img
                 src={src}
                 alt={alt}
-                className="max-w-80 h-auto rounded-lg shadow-md my-2 mx-auto block"
+                className="max-w-2xl h-auto rounded-lg shadow-md my-2 mx-auto block"
                 {...props}
             />
         ),
